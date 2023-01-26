4a1853d04fc6f3aa954317f641c91470ce0c8fe1
krishipaladiya <krishipaladiya@gmail.com>
2023-01-26 10:40:03 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+
+import 'package:e_commerce/homepage.dart';
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: homepage(),
+
+  ));
+}
